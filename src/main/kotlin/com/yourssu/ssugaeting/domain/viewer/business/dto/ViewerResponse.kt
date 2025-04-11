f32c82974a510cce5749c899d2212a66d59ae982
dwl21 <nggus5@gmail.com>
2025-04-12 06:02:51 +0900

    fix: remove ticketNumber field

--- src/main/kotlin/com/yourssu/ssugaeting/domain/viewer/business/dto/ViewerResponse.kt
+++ src/main/kotlin/com/yourssu/ssugaeting/domain/viewer/business/dto/ViewerResponse.kt
@@ -2,7 +2,6 @@ package com.yourssu.ssugaeting.domain.viewer.business.dto
 
 class ViewerResponse(
     val id: Long? = null,
-    val ticketNumber: Int,
     val uuid: String,
     val ticket: Int,
     val usedTicket: Int,

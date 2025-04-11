8b14387cd26c26caf9bb2fe749b72baf9a5d2464
dwl21 <nggus5@gmail.com>
2025-04-12 05:18:03 +0900

    refactor: get myinfo

--- src/main/kotlin/com/yourssu/ssugaeting/domain/viewer/application/ViewerController.kt
+++ src/main/kotlin/com/yourssu/ssugaeting/domain/viewer/application/ViewerController.kt
@@ -1,11 +1,12 @@
 package com.yourssu.ssugaeting.domain.viewer.application
 
 import com.yourssu.ssugaeting.domain.Response
+import com.yourssu.ssugaeting.domain.viewer.application.dto.TicketCreatedRequest
 import com.yourssu.ssugaeting.domain.viewer.application.dto.VerificationRequest
 import com.yourssu.ssugaeting.domain.viewer.application.dto.ViewersFoundRequest
 import com.yourssu.ssugaeting.domain.viewer.business.ViewerService
-import com.yourssu.ssugaeting.domain.viewer.business.dto.ViewerResponse
 import com.yourssu.ssugaeting.domain.viewer.business.dto.VerificationResponse
+import com.yourssu.ssugaeting.domain.viewer.business.dto.ViewerResponse
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -22,7 +23,7 @@ class ViewerController(
     }
 
     @PostMapping
-    fun createTicket(@Valid @RequestBody request: VerificationRequest) : ResponseEntity<Response<ViewerResponse>> {
+    fun createTicket(@Valid @RequestBody request: TicketCreatedRequest) : ResponseEntity<Response<ViewerResponse>> {
         val response = viewerService.issueTicket(request.toCommand())
         return ResponseEntity.ok(Response(result = response))
     }
@@ -30,6 +31,12 @@ class ViewerController(
     @GetMapping
     fun findAllViewers(@Valid @ModelAttribute request: ViewersFoundRequest) : ResponseEntity<Response<List<ViewerResponse>>> {
         val response = viewerService.findAllViewers()
+        return ResponseEntity.ok(Response(result = response))
+    }
+
+    @GetMapping("/me")
+    fun getMyInfo(@Valid @ModelAttribute request: MyInfoRequest): ResponseEntity<Response<ViewerResponse>> {
+        val response = viewerService.getMyInfo()
         return ResponseEntity.ok(Response(result = response))
     }
 }
--- src/main/kotlin/com/yourssu/ssugaeting/domain/viewer/business/dto/ViewerResponse.kt
+++ src/main/kotlin/com/yourssu/ssugaeting/domain/viewer/business/dto/ViewerResponse.kt
@@ -1,6 +1,11 @@
 package com.yourssu.ssugaeting.domain.viewer.business.dto
 
 class ViewerResponse(
+    val id: Long? = null,
     val ticketNumber: Int,
+    val uuid: String,
+    val ticket: Int,
+    val usedTicket: Int,
+    val updatedDate: String,
 ) {
 }

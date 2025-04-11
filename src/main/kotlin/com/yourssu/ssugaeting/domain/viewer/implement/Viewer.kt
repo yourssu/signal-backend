65da385010b9f029051e91502ca0b53baa100469
dwl21 <nggus5@gmail.com>
2025-04-12 06:04:27 +0900

    feat: viewer updatedDate field

--- src/main/kotlin/com/yourssu/ssugaeting/domain/viewer/implement/Viewer.kt
+++ src/main/kotlin/com/yourssu/ssugaeting/domain/viewer/implement/Viewer.kt
@@ -1,9 +1,12 @@
 package com.yourssu.ssugaeting.domain.viewer.implement
 
+import java.time.ZonedDateTime
+
 class Viewer(
     val id: Long? = null,
     val uuid: String,
     val ticket: Int = 0,
     val usedTicket: Int = 0,
+    val updatedDate: ZonedDateTime,
 ) {
 }

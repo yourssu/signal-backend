1de777ad85069845d7a9aeed24f238d6af276432
dwl21 <nggus5@gmail.com>
2025-04-14 21:46:45 +0900

    fix: Int validator

--- src/main/kotlin/com/yourssu/ssugaeting/domain/viewer/application/dto/TicketIssuedRequest.kt
+++ src/main/kotlin/com/yourssu/ssugaeting/domain/viewer/application/dto/TicketIssuedRequest.kt
@@ -2,13 +2,14 @@ package com.yourssu.ssugaeting.domain.viewer.application.dto
 
 import com.yourssu.ssugaeting.domain.viewer.business.dto.TicketIssuedCommand
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
 
 class TicketIssuedRequest(
     @field:NotBlank
     val secretKey: String,
 
-    @field:NotBlank
+    @field:NotNull
     val verificationCode: Int,
 
     @field:Positive

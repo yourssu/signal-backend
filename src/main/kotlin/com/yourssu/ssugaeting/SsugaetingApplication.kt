29afae015394e629c3cf3ad3afd36cbc69343c09
dwl21 <nggus5@gmail.com>
2025-04-14 15:17:02 +0900

    chore: ConfigurationPropertiesScan

--- src/main/kotlin/com/yourssu/ssugaeting/SsugaetingApplication.kt
+++ src/main/kotlin/com/yourssu/ssugaeting/SsugaetingApplication.kt
@@ -1,10 +1,12 @@
 package com.yourssu.ssugaeting
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class SsugaetingApplication
 

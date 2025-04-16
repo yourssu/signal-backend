055eb1003f7e4936c2e0954d0964ca72a559e926
dwl21 <nggus5@gmail.com>
2025-04-17 01:31:22 +0900

    feat: mismatch gender 409

---
+++ src/main/kotlin/com/yourssu/ssugaeting/domain/profile/implement/exception/GenderMismatchException.kt
@@ -0,0 +1,6 @@
+package com.yourssu.ssugaeting.domain.profile.implement.exception
+
+import com.yourssu.ssugaeting.handler.ConflictException
+
+class GenderMismatchException : ConflictException(message = "성별이 일치하지 않습니다.") {
+}

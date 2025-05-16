304965f5bb3ac526e7f4b19ecdecf9932c14fd4c
dwl21 <nggus5@gmail.com>
2025-05-16 23:24:53 +0900

    feat: max size of department

--- src/main/kotlin/com/yourssu/signal/domain/profile/application/dto/ProfileCreatedRequest.kt
+++ src/main/kotlin/com/yourssu/signal/domain/profile/application/dto/ProfileCreatedRequest.kt
@@ -11,7 +11,7 @@ data class ProfileCreatedRequest(
     @field:NotBlank
     val gender: String,
 
-//    @field:NotBlank
+    @field:Size(min = 1, max = 20)
     val department: String = "학과",
 
 //    @field:Min(1900)

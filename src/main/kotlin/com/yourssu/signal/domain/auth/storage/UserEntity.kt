69edfc3eea8e442bf28ce2affbd23b5e51a89695
dwl21 <nggus5@gmail.com>
2025-08-25 00:25:38 +0900

    [#5]; feat: users 엔티티 설계

--- src/main/kotlin/com/yourssu/signal/domain/auth/storage/UserEntity.kt
+++ src/main/kotlin/com/yourssu/signal/domain/auth/storage/UserEntity.kt
@@ -8,7 +8,9 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
+@Table(name = "users")
 @Entity
 class UserEntity(
     @Id

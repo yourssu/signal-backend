dfe6873b24aa6ca29a2e93ce94056963cf0db6b2
dwl21 <nggus5@gmail.com>
2025-08-25 01:06:37 +0900

    [#7]; feat: 토큰 인증 인가 방식 적용

--- src/main/kotlin/com/yourssu/signal/domain/profile/application/ProfileController.kt
+++ src/main/kotlin/com/yourssu/signal/domain/profile/application/ProfileController.kt
@@ -6,11 +6,13 @@ import com.yourssu.signal.domain.common.business.dto.Response
 import com.yourssu.signal.domain.profile.application.dto.*
 import com.yourssu.signal.domain.profile.business.ProfilesCountResponse
 import com.yourssu.signal.domain.profile.business.ProfileService
+import com.yourssu.signal.domain.profile.business.command.ProfileFoundCommand
 import com.yourssu.signal.domain.profile.business.dto.MyProfileResponse
 import com.yourssu.signal.domain.profile.business.dto.ProfileContactResponse
 import com.yourssu.signal.domain.profile.business.dto.ProfileResponse
 import com.yourssu.signal.domain.viewer.application.dto.RandomProfileRequest
 import jakarta.validation.Valid
+import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -21,18 +23,24 @@ class ProfileController(
     private val profileService: ProfileService,
 ) {
     @PostMapping
-    fun createProfile(@Valid @RequestBody request: ProfileCreatedRequest): ResponseEntity<Response<MyProfileResponse>> {
-        val response = profileService.createProfile(request.toCommand())
+    @RequireAuth
+    fun createProfile(
+        @Valid @RequestBody request: ProfileCreatedRequest,
+        @UserUuid uuid: String
+    ): ResponseEntity<Response<MyProfileResponse>> {
+        val response = profileService.createProfile(request.toCommand(uuid))
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(Response(result = response))
     }
 
     @GetMapping("/uuid")
+    @RequireAuth
     fun getProfile(@UserUuid uuid: String): ResponseEntity<Response<MyProfileResponse>> {
         val response = profileService.getProfile(uuid)
         return ResponseEntity.ok(Response(result = response))
     }
 
+    @Profile("!prod")
     @GetMapping
     fun getAllProfiles(@Valid @ModelAttribute request: ProfilesFoundRequest): ResponseEntity<Response<List<ProfileResponse>>> {
         val response = profileService.getAllProfiles(request.toCommand())
@@ -52,20 +60,26 @@ class ProfileController(
     }
 
     @GetMapping("/{profileId}")
-    fun getProfile(@Valid @ModelAttribute request: ProfileFoundRequest, @PathVariable profileId: Long): ResponseEntity<Response<ProfileContactResponse>> {
-        val response = profileService.getProfile(request.toCommand(profileId))
+    @RequireAuth
+    fun getProfile(
+        @UserUuid uuid: String,
+        @PathVariable profileId: Long
+    ): ResponseEntity<Response<ProfileContactResponse>> {
+        val response = profileService.getProfile(ProfileFoundCommand(profileId = profileId, uuid = uuid))
         return ResponseEntity.ok(Response(result = response))
     }
 
     @GetMapping("/random")
-    fun getRandomProfile(@Valid @ModelAttribute request: RandomProfileRequest): ResponseEntity<Response<ProfileResponse>> {
-        val response = profileService.getRandomProfile(request.toCommand())
+    @RequireAuth
+    fun getRandomProfile(@Valid @ModelAttribute request: RandomProfileRequest, @UserUuid uuid: String): ResponseEntity<Response<ProfileResponse>> {
+        val response = profileService.getRandomProfile(request.toCommand(uuid))
         return ResponseEntity.ok(Response(result = response))
     }
 
     @PostMapping("/contact")
-    fun consumeTicket(@Valid @RequestBody request: TicketConsumedRequest): ResponseEntity<Response<ProfileContactResponse>> {
-        val response = profileService.consumeTicket(request.toCommand())
+    @RequireAuth
+    fun consumeTicket(@Valid @RequestBody request: TicketConsumedRequest, @UserUuid uuid: String): ResponseEntity<Response<ProfileContactResponse>> {
+        val response = profileService.consumeTicket(request.toCommand(uuid))
         return ResponseEntity.ok(Response(result = response))
     }
 }

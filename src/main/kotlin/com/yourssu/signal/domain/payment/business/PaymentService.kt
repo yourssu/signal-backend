bd53cd5860714688a3b226e60dd497eb60ded989
dwl21 <nggus5@gmail.com>
2025-08-24 18:57:43 +0900

    [#2]; feat: 결제 승인 티켓 발급 기능 구현

--- src/main/kotlin/com/yourssu/signal/domain/payment/business/PaymentService.kt
+++ src/main/kotlin/com/yourssu/signal/domain/payment/business/PaymentService.kt
@@ -7,14 +7,15 @@ import com.yourssu.signal.domain.payment.business.dto.PaymentInitiationResponse
 import com.yourssu.signal.domain.payment.implement.KakaoPayOrderReader
 import com.yourssu.signal.domain.payment.implement.PaymentManager
 import com.yourssu.signal.domain.viewer.implement.ViewerReader
+import com.yourssu.signal.domain.viewer.implement.ViewerWriter
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional
 class PaymentService(
     private val paymentManager: PaymentManager,
     private val viewerReader: ViewerReader,
+    private val viewerWriter: ViewerWriter,
     private val kakaoPayOrderReader: KakaoPayOrderReader,
 ) {
     fun initiate(command: PaymentInitiationCommand): PaymentInitiationResponse {
@@ -25,10 +26,12 @@ class PaymentService(
         )
         return PaymentInitiationResponse.from(result)
     }
-    
+
+    @Transactional
     fun approve(command: PaymentApprovalCommand): PaymentCompletionResponse {
         val viewer = viewerReader.get(command.toUuid())
         val kakaoPayOrder = kakaoPayOrderReader.getByViewerAndTid(viewer = viewer, tid = command.tid)
+        viewerWriter.issueTicket(viewer.uuid, kakaoPayOrder.quantity)
         val result = paymentManager.approve(
             kakaoPayOrder = kakaoPayOrder,
             pgToken = command.pgToken

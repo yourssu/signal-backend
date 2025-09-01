eea08c11abcccbae4f4f20ef8f03c01b518b999b
dwl21 <nggus5@gmail.com>
2025-09-02 00:24:53 +0900

    [#29]; feat: 나의 블랙리스트 추가, 삭제 구현

--- src/main/kotlin/com/yourssu/signal/domain/blacklist/business/BlacklistService.kt
+++ src/main/kotlin/com/yourssu/signal/domain/blacklist/business/BlacklistService.kt
@@ -6,9 +6,12 @@ import com.yourssu.signal.domain.blacklist.business.dto.BlacklistExistsResponse
 import com.yourssu.signal.domain.blacklist.business.dto.BlacklistResponse
 import com.yourssu.signal.domain.blacklist.implement.BlacklistReader
 import com.yourssu.signal.domain.blacklist.implement.BlacklistWriter
+import com.yourssu.signal.domain.blacklist.implement.domain.Blacklist
+import com.yourssu.signal.domain.blacklist.implement.exception.BlacklistAlreadyExistsException
 import com.yourssu.signal.domain.common.implement.Uuid
 import com.yourssu.signal.domain.profile.implement.ProfileReader
 import com.yourssu.signal.domain.viewer.implement.AdminAccessChecker
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
@@ -38,5 +41,27 @@ class BlacklistService(
         val profile = profileReader.getByUuid(Uuid(uuid))
         val isBlacklisted = blacklistReader.existsByProfileId(profile.id!!)
         return BlacklistExistsResponse(isBlacklisted = isBlacklisted)
+    }
+
+    fun addMyBlacklist(uuid: String): BlacklistResponse {
+        val profile = profileReader.getByUuid(Uuid(uuid))
+        if (blacklistReader.existsByProfileId(profile.id!!)) {
+            throw BlacklistAlreadyExistsException()
+        }
+        val blacklist = blacklistWriter.save(
+            Blacklist(
+                profileId = profile.id,
+                createdByAdmin = false
+            )
+        )
+        return BlacklistResponse.from(blacklist)
+    }
+
+    @Transactional
+    fun removeMyBlacklist(uuid: String) {
+        val profile = profileReader.getByUuid(Uuid(uuid))
+        if (blacklistReader.existsByProfileId(profile.id!!)) {
+            blacklistWriter.deleteByProfileId(profile.id)
+        }
     }
 }

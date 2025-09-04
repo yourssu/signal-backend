1976cf3ada690eb5d714913ad12272912c1d41b2
dwl21 <nggus5@gmail.com>
2025-09-04 10:36:41 +0900

    [#41]; feat: KB 국민은행 sms 입금 문자를 파싱한다.

--- src/main/kotlin/com/yourssu/signal/infrastructure/deposit/SMSMessage.kt
+++ src/main/kotlin/com/yourssu/signal/infrastructure/deposit/SMSMessage.kt
@@ -3,5 +3,6 @@ package com.yourssu.signal.infrastructure.deposit
 data class SMSMessage(
     val depositAmount: Int,
     val name: String,
+    val remainingAmount: Int? = null
 ) {
 }
--- src/main/kotlin/com/yourssu/signal/infrastructure/deposit/SMSParser.kt
+++ src/main/kotlin/com/yourssu/signal/infrastructure/deposit/SMSParser.kt
@@ -4,6 +4,7 @@ import com.yourssu.signal.infrastructure.deposit.exception.InvalidBankException
 
 private const val KAKAO_BANK_SMS = "kakao_sms"
 private const val KAKAO_BANK_TALK = "kakao_talk"
+private const val KB_BANK_SMS = "kb_sms"
 
 interface SMSParser {
     fun parse(message: String): SMSMessage
@@ -13,6 +14,7 @@ interface SMSParser {
             return when (type) {
                 KAKAO_BANK_SMS -> KakaoBankSMSParser
                 KAKAO_BANK_TALK -> KakaoBankTalkParser
+                KB_BANK_SMS -> KbBankSMSParser
                 else -> throw InvalidBankException(type)
             }
         }

77ac2806b4484bb1189ffe6611051250a071145a
dwl21 <nggus5@gmail.com>
2025-09-04 10:47:44 +0900

    [#41]; refactor: 타입 선택에 다형성을 활용한다.

--- src/main/kotlin/com/yourssu/signal/infrastructure/deposit/SMSParser.kt
+++ src/main/kotlin/com/yourssu/signal/infrastructure/deposit/SMSParser.kt
@@ -2,21 +2,20 @@ package com.yourssu.signal.infrastructure.deposit
 
 import com.yourssu.signal.infrastructure.deposit.exception.InvalidBankException
 
-private const val KAKAO_BANK_SMS = "kakao_sms"
-private const val KAKAO_BANK_TALK = "kakao_talk"
-private const val KB_BANK_SMS = "kb_sms"
-
 interface SMSParser {
+    val type: String
+
     fun parse(message: String): SMSMessage
 
     companion object {
+        private val parsers: List<SMSParser> = listOf(
+            KakaoBankSMSParser,
+            KbBankSMSParser
+        )
+
         fun of(type: String): SMSParser {
-            return when (type) {
-                KAKAO_BANK_SMS -> KakaoBankSMSParser
-                KAKAO_BANK_TALK -> KakaoBankTalkParser
-                KB_BANK_SMS -> KbBankSMSParser
-                else -> throw InvalidBankException(type)
-            }
+            return parsers.find { it.type == type }
+                ?: throw InvalidBankException(type)
         }
     }
 }

da1b6c1ef86c152e50ad70b0080e61895365b11d
dwl21 <nggus5@gmail.com>
2025-09-05 16:04:42 +0900

    [#52]; feat: 입금 문자 전송 기능의 타입과 시크릿 키를 pathVariable로 받을 수 있다.

---
+++ src/main/kotlin/com/yourssu/signal/api/dto/BankDepositSmsMessageRequest.kt
@@ -0,0 +1,17 @@
+package com.yourssu.signal.api.dto
+
+import com.yourssu.signal.domain.viewer.business.command.ProcessDepositSmsCommand
+import jakarta.validation.constraints.NotBlank
+
+class BankDepositSmsMessageRequest(
+    @field:NotBlank
+    val message: String,
+) {
+    fun toCommand(secretKey: String, type: String): ProcessDepositSmsCommand {
+        return ProcessDepositSmsCommand(
+            secretKey = secretKey,
+            message = message,
+            type = type,
+        )
+    }
+}

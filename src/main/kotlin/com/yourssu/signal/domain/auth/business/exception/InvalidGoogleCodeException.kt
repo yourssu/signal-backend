beb76a292bffd228defb8289c07b722ed5ec9425
dwl21 <nggus5@gmail.com>
2025-09-10 18:36:46 +0900

    [no issued]; refactor: 구글 로그인 실패 예외 문구 변경

--- src/main/kotlin/com/yourssu/signal/domain/auth/business/exception/InvalidGoogleCodeException.kt
+++ src/main/kotlin/com/yourssu/signal/domain/auth/business/exception/InvalidGoogleCodeException.kt
@@ -2,5 +2,5 @@ package com.yourssu.signal.domain.auth.business.exception
 
 import com.yourssu.signal.handler.UnauthorizedException
 
-class InvalidGoogleCodeException : UnauthorizedException(message = "구글 로그인에 실패했습니다. 다시 시도해주세요.") {
+class InvalidGoogleCodeException : UnauthorizedException(message = "구글 로그인에 실패했습니다. 잠시 후 다시 시도해주세요.") {
 }

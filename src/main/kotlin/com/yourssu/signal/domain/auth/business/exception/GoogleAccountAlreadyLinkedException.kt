1efc11a8eb184823c2cff148fc152b29ac8cf837
dwl21 <nggus5@gmail.com>
2025-09-10 14:33:14 +0900

    [no issued]; refactor: 구글 로그인 에러 문구 클라이언트 용으로 수정

--- src/main/kotlin/com/yourssu/signal/domain/auth/business/exception/GoogleAccountAlreadyLinkedException.kt
+++ src/main/kotlin/com/yourssu/signal/domain/auth/business/exception/GoogleAccountAlreadyLinkedException.kt
@@ -2,5 +2,5 @@ package com.yourssu.signal.domain.auth.business.exception
 
 import com.yourssu.signal.handler.ConflictException
 
-class GoogleAccountAlreadyLinkedException: ConflictException(message = "Google account already linked") {
+class GoogleAccountAlreadyLinkedException: ConflictException(message = "구글 계정이 이미 다른 계정과 연동되어 있습니다. 다른 구글 계정을 사용해주세요.") {
 }
--- src/main/kotlin/com/yourssu/signal/domain/auth/business/exception/InvalidGoogleCodeException.kt
+++ src/main/kotlin/com/yourssu/signal/domain/auth/business/exception/InvalidGoogleCodeException.kt
@@ -2,4 +2,5 @@ package com.yourssu.signal.domain.auth.business.exception
 
 import com.yourssu.signal.handler.UnauthorizedException
 
-class InvalidGoogleCodeException : UnauthorizedException(message = "Invalid a code for Google")
+class InvalidGoogleCodeException : UnauthorizedException(message = "구글 로그인에 실패했습니다. 다시 시도해주세요.") {
+}
